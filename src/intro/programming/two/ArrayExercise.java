f48f0639fbf6ab0e95a5a13fe344dd73490695ba
Brian Knight <brian.knight@siliconmtn.com>
2023-07-12 10:48:49 -0600

    Adds Intro to Programming Part II exexercise.

---
+++ src/intro/programming/two/ArrayExercise.java
@@ -0,0 +1,45 @@
+package intro.programming.two;
+
+import java.util.Arrays;
+
+/****************************************************************************
+ * <b>Title:</b> ArraysExercise.java
+ * <b>Project:</b> brian.training
+ * <b>Description:</b> CHANGE ME!!
+ * <b>Copyright:</b> Copyright (c) 2023
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author Brian Knight
+ * @version 3.x
+ * @since Jul 12, 2023
+ * <b>updates:</b>
+ *  
+ ****************************************************************************/
+
+public class ArrayExercise {
+	private int[] nums;
+	
+	public ArrayExercise() {
+		nums = new int[10];
+		fillArray();
+	}
+	
+	public ArrayExercise(int n) {
+		nums = new int[n];
+		fillArray();
+	}
+	
+	public void display() {
+		System.out.printf("%d random numbers%n", nums.length);
+		for (int i=0; i<nums.length; i++) {
+			System.out.println(String.valueOf(i+1) + ". " + nums[i]);
+		}
+	}
+	
+	private void fillArray() {
+		for (int i=0; i<nums.length; i++ ) {
+			nums[i] = (int) (Math.random() * 100); 
+		}
+	}
+		
+}
---
+++ src/intro/programming/two/ArrayExerciseDemo.java
@@ -0,0 +1,27 @@
+package intro.programming.two;
+
+/****************************************************************************
+ * <b>Title:</b> ArrayExerciseDemo.java
+ * <b>Project:</b> brian.training
+ * <b>Description:</b> CHANGE ME!!
+ * <b>Copyright:</b> Copyright (c) 2023
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author Brian Knight
+ * @version 3.x
+ * @since Jul 12, 2023
+ * <b>updates:</b>
+ *  
+ ****************************************************************************/
+
+public class ArrayExerciseDemo {
+	public static void main(String[] args) {
+		ArrayExercise e1 = new ArrayExercise();
+		e1.display();
+		
+		System.out.println("*".repeat(50));
+		
+		ArrayExercise e2 = new ArrayExercise(20);
+		e2.display();
+	}
+}
