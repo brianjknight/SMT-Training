51c6a805b07afae380309f1232ad159114e4ce34
Brian Knight <brian.knight@siliconmtn.com>
2023-09-22 07:40:53 -0600

    Creates method for post request

--- src/webcrawler/HtmlFetcher.java
+++ src/webcrawler/HtmlFetcher.java
@@ -59,34 +59,10 @@ public class HtmlFetcher {
 		
 		// Use socket to send POST request to login and retrieve cookies.
         try (Socket socket = createSocket(url)) {
-    		String postRequestBody = "requestType=" + URLEncoder.encode("reqBuild", "UTF-8") +
-    				"&pmid=" + URLEncoder.encode("ADMIN_LOGIN", "UTF-8") +
-    				"&emailAddress=" + URLEncoder.encode(username, "UTF-8") +
-    				"&password=" + URLEncoder.encode(password, "UTF-8") +
-    				"&l=" + URLEncoder.encode("", "UTF-8");
-    		
     		OutputStream out = socket.getOutputStream();
     		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     		
-    		out.write(("POST " + url.getFile() + " HTTP/1.1\r\n").getBytes());
-    		out.write(("Host: " + url.getHost() + "\r\n").getBytes());
-    		out.write(("Content-Length: " + postRequestBody.length() + "\r\n").getBytes());		
-    		out.write("Content-Type: application/x-www-form-urlencoded\r\n".getBytes());
-    		out.write("\r\n".getBytes());
-    		out.write((postRequestBody + "\r\n").getBytes());
-    		out.write("Connection: close\r\n\r\n".getBytes());
-    		out.flush();
-    		
-    		// Save cookies
-    		String line = "";
-    		while ((line = in.readLine()) != null) {
-    			if (!cookies.keySet().contains(url.getHost())) {
-    				cookies.put(url.getHost(), new HashSet<>());
-    			}
-    			if (line.contains("Cookie")) {
-    				cookies.get(url.getHost()).add(line);
-    			}
-    		}
+    		sendPostRequest(socket, out, in, url, username, password);
     		
     		socket.close();
     		in.close();       	
@@ -95,6 +71,7 @@ public class HtmlFetcher {
             System.out.println(e);
         }
         
+        // TODO refactor as helper method to call in fetchHtml
         // Once cookies are available send GET request to retrieve the HTML
         try (Socket socket = createSocket(url)) {
         	OutputStream out = socket.getOutputStream();
@@ -155,4 +132,55 @@ public class HtmlFetcher {
 			return new Socket(url.getHost(), url.getDefaultPort());
 		}
 	}
+	
+	/**
+	 * Uses a socket to send a post request with login credentials and retrieve cookies for a session.
+	 * @param socket
+	 * @param out
+	 * @param url
+	 * @param username
+	 * @param password
+	 * @throws IOException
+	 */
+	private void sendPostRequest(Socket socket, OutputStream out, BufferedReader in, URL url, String username, String password) throws IOException {
+        String postRequestBody = "requestType=" + URLEncoder.encode("reqBuild", "UTF-8") +
+                "&pmid=" + URLEncoder.encode("ADMIN_LOGIN", "UTF-8") +
+                "&emailAddress=" + URLEncoder.encode(username, "UTF-8") +
+                "&password=" + URLEncoder.encode(password, "UTF-8") +
+                "&l=" + URLEncoder.encode("", "UTF-8");
+
+        String requestHeaders = "POST " + url.getFile() + " HTTP/1.1\r\n" +
+                "Host: " + url.getHost() + "\r\n" +
+                "Content-Length: " + postRequestBody.length() + "\r\n" +
+                "Content-Type: application/x-www-form-urlencoded\r\n" +
+                "\r\n" + postRequestBody + "\r\n" +
+                "Connection: close\r\n\r\n";
+
+        out.write(requestHeaders.getBytes());
+        out.flush();
+        
+        // save cookies
+        String line;
+        while ((line = in.readLine()) != null) {
+            if (!cookies.containsKey(url.getHost())) {
+                cookies.put(url.getHost(), new HashSet<>());
+            }
+            if (line.contains("Cookie")) {
+                cookies.get(url.getHost()).add(line);
+            }
+        }
+    }
+	
+	/**
+	 * Uses a socket to send a GET request with cookies to retrieve HTML of a webpage.
+	 * @param socket
+	 * @param out
+	 * @param in
+	 * @param url
+	 * @param html
+	 * @throws IOException
+	 */
+	private void sendGetRequest(Socket socket, OutputStream out, BufferedReader in, URL url, StringBuilder html) throws IOException {
+		// TODO create method
+    }
 }
