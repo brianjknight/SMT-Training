8848de84dd61bde70c4c179c8dc25ea09a4c72c3
Brian Knight <brian.knight@siliconmtn.com>
2024-04-04 15:58:37 -0600

    clear test class

--- src/myTestPackage/MyTestClass.java
+++ src/myTestPackage/MyTestClass.java
@@ -1,42 +1,7 @@
 package myTestPackage;
 
-import java.util.HashMap;
-import java.util.Map;
-
-/****************************************************************************
- * <b>Title:</b> MyTestClass.java
- * <b>Project:</b> brian.training
- * <b>Description:</b> CHANGE ME!!
- * <b>Copyright:</b> Copyright (c) 2023
- * <b>Company:</b> Silicon Mountain Technologies
- * 
- * @author Brian Knight
- * @version 3.x
- * @since Oct 24, 2023
- * <b>updates:</b>
- *  
- ****************************************************************************/
+import java.util.*;
 
 public class MyTestClass {
-
-	public static void main(String[] args) {
-		
-		Map<String, Double> one = new HashMap<>();
-		one.put("A", 1.0);
-		one.put("B", 1.0);
-		one.put("C", 1.0);
-		
-		Map<String, Double> two = new HashMap<>();
-		two.put("A", 1.0);
-		two.put("B", 1.0);
-		two.put("C", 1.0);
-		two.put("D", 1.0);
-		two.put("E", 1.0);
-		
-		Map<String, Double> combined = new HashMap<>(one);
-		two.forEach((key, value) -> combined.merge(key, value, Double::sum));
-		
-		System.out.println(combined);
-		
-	}
+	
 }
